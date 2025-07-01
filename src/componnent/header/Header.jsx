bf77dbf05de48e9df9ee8c5d350a08dac6248943
abushe136 <amir.mesfin136@gmail.com>
2025-07-01 02:35:54 -0300

    header worke

---
+++ src/componnent/header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+     <section>
+         <section>
+            <div>
+              {/* // logo */}
+               <a href="">
+                   <img src="../../assets/image/DessieMartLogo.png" alt="" />
+               </a>
+                
+            </div>
+            <div></div>
+            <div></div>
+         </section>
+     </section>
+  )
+}
