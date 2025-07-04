a1e74231d6a17408a22ff531c9342cf27e8dbf25
abushe136 <amir.mesfin136@gmail.com>
2025-07-04 20:21:39 -0300

    add loadinig future in thecatagory page

--- src/component/catagory/Catagory.jsx
+++ src/component/catagory/Catagory.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import styles from './Catagory.module.css';
 import CatagoryCard from './CatagoryCard';
+import FadeLoaderComponent from '../Loader/FadeLoaderComponent';
 
 function Catagory() {
   const [categories, setCategories] = useState([]);
   const [filteredData, setFilteredData] = useState([]);
-
+  const [loader,setLoader]=useState(true);
   useEffect(() => {
     fetch('https://dummyjson.com/products/categories')
       .then(res => res.json())
@@ -24,21 +25,31 @@ function Catagory() {
                 linkUrl: cat.url
               }))
           )
-        ).then(results => setFilteredData(results));
+        ).then(results => setFilteredData(results))
+        .catch(err=>{
+          setLoader(false);
+          console.log(err);
+        })
+        .finally(()=>{
+          setLoader(false);
+        });
       });
   }, []);
 
   return (
-    <div className={styles.catagoryWrapper}>
-      {filteredData.map((item, index) => (
-        <CatagoryCard
-          key={index}
-          title={item.title}       // Pass full category object
-          products={item.products}
-          linkUrl={item.linkUrl}
-        />
-      ))}
-    </div>
+    <>
+        {loader? (<FadeLoaderComponent />):( <div className={styles.catagoryWrapper}>
+                                                {filteredData.map((item, index) => (
+                                                  <CatagoryCard
+                                                    key={index}
+                                                    title={item.title}       // Pass full category object
+                                                    products={item.products}
+                                                    linkUrl={item.linkUrl}
+                                                  />
+                                                ))}
+                                              </div>)} 
+    </>
+    
   );
 }
 
